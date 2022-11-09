68ad37d1f79fe204456b11398eba08ac47c00d38
AllenCheung0213 <alcheung0213@gmail.com>
2022-11-09 14:55:39 -0500

    made some new stuff

--- RomaniaCityApp.py
+++ RomaniaCityApp.py
@@ -1,5 +1,12 @@
+import SimpleProblemSolvingAgent
+from SimpleProblemSolvingAgent import SimpleProblemSolvingAgentProgram
+from romania_map import romania_map
+
 def main():
+    agent = SimpleProblemSolvingAgent.SimpleProblemSolvingAgentProgram('Arad', 'Bucharest')
+    print(agent.best_first_search())
+    print('j')
     pass
 
-if __name__ =="_main_":
+if __name__ =="__main__":
     main()
--- SimpleProblemSolvingAgent.py
+++ SimpleProblemSolvingAgent.py
@@ -1,6 +1,9 @@
 import queue
+from queue import *
+from romania_map import romania_map
 from utils import *
 from graph import *
+
 
 class SimpleProblemSolvingAgentProgram:
     """
@@ -17,31 +20,59 @@ class SimpleProblemSolvingAgentProgram:
         self.seq = []
 
     def __call__(self):
-        """[Figure 3.1] Formulate a goal and problem, then
-        search for a sequence of actions to solve it."""
-        if not self.seq:
-            if (self.goal!=None and self.state!=None):
-                goal = self.goal
-                self.seq = self.search(self)
-                if not self.seq:
-                    return None
-            return self.seq.pop(0)
-        #return
+        print('hi')
+        # """[Figure 3.1] Formulate a goal and problem, then
+        # search for a sequence of actions to solve it."""
+        # if not self.seq:
+        #     if (self.goal!=None and self.state!=None):
+        #         goal = self.goal
+        #         self.seq = self.search(self)
+        #         if not self.seq:
+        #             return None
+        #     return self.seq.pop(0)
+        # #return
 
     def search(self):
         raise NotImplementedError
 
-    def best_first_search(start, target, n):
+    def best_first_search(self):
+        start = self.state
+        target = self.goal
         graph = createGraph()
-        visited = [False] * n
-        pq = PriorityQueue()
+
+        city_names = ["Arad",
+                      "Bucharest",
+                      "Craiova",
+                      "Dobreta",
+                      "Eforie",
+                      "Fagaras",
+                      "Giurgiu",
+                      "Hirsova",
+                      "Iasi",
+                      "Lugoj",
+                      "Mehadia",
+                      "Neamt",
+                      "Oradea",
+                      "Pitesti",
+                      "Rimnicu_Vilcea",
+                      "Sibiu",
+                      "Timisoara",
+                      "Urziceni",
+                      "Vaslui",
+                      "Zerind"]
+
+        visited = {}
+        for city in city_names:
+            visited[city] = False
+
+        pq = queue.PriorityQueue()
         pq.put((0, start))
         visited[start] = True
 
-        while not pq.empty():
+        while pq.not_empty:
             current = pq.get()[1]
             # Displaying the path having lowest cost
-            print(current, end=" ")
+            #print(current, end=" ")
             if current == target:
                 break
 
@@ -49,7 +80,7 @@ class SimpleProblemSolvingAgentProgram:
                 if not visited[city]:
                     visited[city] = True
                     pq.put((cost, city))
-        print()
+        print(pq.queue)
 
     def astar_search(self, h=None, display=False):
         """A* search is best-first graph search with f(n) = g(n)+h(n).
@@ -58,3 +89,12 @@ class SimpleProblemSolvingAgentProgram:
         h = memoize(h or self.h, 'h')
         return self.best_first_graph_search(self, lambda n: n.path_cost + h(n), display)
 
+# def main():
+#     agent = SimpleProblemSolvingAgent('Arad', 'Bucharest')
+#     # agent.SimpleProblemSolvingAgentProgram.best_first_search()
+#     print(agent.state)
+#     # print(bfs)
+#     print('j')
+#
+# if __name__ =="__main__":
+#     main()
